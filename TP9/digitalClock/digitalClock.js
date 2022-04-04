d7a477cd62cf1a8bfc4164b97f2c49082f4d5a3a
cfouche3005 <cfouche3005@gmail.com>
2022-04-04 17:25:44 +0200

    TP9

--- TP9/digitalClock/digitalClock.js
+++ TP9/digitalClock/digitalClock.js
@@ -1,5 +1,9 @@
 ﻿function addSegments(digitId){
-
+    let digitDiv = document.getElementById(digitId);
+    for (let i = 0; i <7; i++){
+        let html =`<div class="segment segment${i} off"></div>`;
+        digitDiv.insertAdjacentHTML('beforeend', html);
+    }
     // TODO : compléter
 
 }
@@ -18,23 +22,60 @@ function updateDigit(digitId, value){
         [1, 1, 1, 1, 1, 1, 1],
         [1, 1, 1, 1, 0, 1, 1]
     ];
-
+    let digitDiv = document.getElementById(digitId);
+    for (let i = 0; i <7; i++){
+        if (segmentStates[value][i]==1){
+            let html =`segment segment${i} on`;
+            digitDiv.children[i].className=html;
+        }else{
+            let html =`segment segment${i} off`;
+            digitDiv.children[i].className=html;
+        }
+    }
     // TODO : compléter
 }
 
+function updateDigitTime(){
+    let now = new Date();
+    let hourstens = String(now.getHours())[0];
+    let hoursunits = String(now.getHours())[1];
+    let  minutestens = String(now.getMinutes())[0];
+    let  minutesunits = String(now.getMinutes())[1];
 
+    updateDigit("hours-tens",hourstens);
+    updateDigit("hours-units",hoursunits);
+    updateDigit("minutes-tens",minutestens);
+    updateDigit("minutes-units",minutesunits);
 
+}
+
+function updatePoint(){
+    let now = new Date();
+    let seconds = now.getSeconds();
+    let digitpoint = document.getElementById("colon");
+    if (seconds%2==0){
+        let html =`colon off`;
+        digitpoint.className=html;
+    }else{
+        let html =`colon on`;
+        digitpoint.className=html;
+    }
+
+}
 
 function init(){
-
+    addSegments("hours-tens");
+    addSegments("hours-units");
+    addSegments("minutes-tens");
+    addSegments("minutes-units");
     // TODO : compléter
 }
-
-
 
 function main(){
 
     init();
+    setInterval(updateDigitTime,1000);
+    setInterval(updatePoint,1000)
 
     // TODO : compléter
 }
