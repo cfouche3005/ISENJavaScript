ef5e56994e118a29a5d3ca8326921714760f0a85
cfouche3005 <cfouche3005@gmail.com>
2022-04-06 14:10:32 +0200

    TP10

--- TP10/events/colorbutton/colorbutton.js
+++ TP10/events/colorbutton/colorbutton.js
@@ -1,4 +1,18 @@
-function main(){
+function randomComponent() {
+    return Math.floor(Math.random() * 255);
+}
+
+function randomColor() {
+    return `rgb(${randomComponent()},${randomComponent()},${randomComponent()})`
+}
+
+function changeColor(event) {
+    event.target.style.backgroundColor = randomColor();
+}
+
+function main() {
+    let colorButton = document.getElementById("colorButton");
+    colorButton.addEventListener("click", changeColor);
 
 }
 
