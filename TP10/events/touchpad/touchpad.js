4bbdb5151ff43b33a5b12a5cc61408acb2a94c54
cfouche3005 <cfouche3005@gmail.com>
2022-04-06 15:19:48 +0200

    TP10

--- TP10/events/touchpad/touchpad.js
+++ TP10/events/touchpad/touchpad.js
@@ -1,4 +1,27 @@
+function touch(event){
+    let rect = event.target.getBoundingClientRect()
+    let x = (event.clientX-rect.left)/(rect.right-rect.left)*100;
+    let y = (event.clientY-rect.top)/(rect.bottom-rect.top)*100;
+    console.log(x,y);
+    let screen = document.getElementById("screen");
+    let screenContext = screen.getContext("2d");
+    screenContext.clearRect(0,0,screen.width,screen.height)
+    screenContext.beginPath();
+    screenContext.fillStyle="grey";
+    screenContext.arc(x*rect.width, y*rect.height, 30,0,Math.PI*2);
+    screenContext.closePath();
+    screenContext.fill();
+
+
+
+}
+
+
+
+
 function main(){
+    let touchpad = document.getElementById("touchpad");
+    touchpad.addEventListener('mousemove',touch);
 
 }
 
